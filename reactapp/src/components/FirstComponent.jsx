0f71c5c1ed970cd7b125f3c342e23832106ff215
milos-jolovic <mj20200179@student.fon.bg.ac.rs>
2024-11-30 23:19:34 +0100

    Dodat p tag da napravimo konflikt

--- reactapp/src/components/FirstComponent.jsx
+++ reactapp/src/components/FirstComponent.jsx
@@ -5,7 +5,7 @@ const FirstComponent = () => {
     <div>
         <h1>Hello world with react and git!</h1>
         <p>Ako i ovde stavim promene neke vrste i sacuvam fajl on ce da predje u modified i da se pokaze na git diff!</p>
-
+        <p>Ovo je neki p tag</p>
     </div>
   )
 }
