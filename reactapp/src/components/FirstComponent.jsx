f0857ecce683e8007854df1e96f67e558011ca24
milos-jolovic <mj20200179@student.fon.bg.ac.rs>
2024-11-30 23:20:09 +0100

    Dodat h tag da napravimo konflikt

--- reactapp/src/components/FirstComponent.jsx
+++ reactapp/src/components/FirstComponent.jsx
@@ -5,7 +5,7 @@ const FirstComponent = () => {
     <div>
         <h1>Hello world with react and git!</h1>
         <p>Ako i ovde stavim promene neke vrste i sacuvam fajl on ce da predje u modified i da se pokaze na git diff!</p>
-
+        <h1>E ovo je h1 tag</h1>
     </div>
   )
 }
