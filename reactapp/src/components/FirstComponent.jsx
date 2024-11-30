9302e8b3391f9b48e94382c4f5263a926be23c6b
milos-jolovic <mj20200179@student.fon.bg.ac.rs>
2024-11-30 20:11:58 +0100

    Ovo je nas prvi commit

---
+++ reactapp/src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FirstComponent = () => {
+  return (
+    <div>Hello world with react and git!</div>
+  )
+}
+
+export default FirstComponent
---
+++ reactapp/src/components/SecondComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SecondComponent = () => {
+  return (
+    <div>SecondComponent that also loves git and github!</div>
+  )
+}
+
+export default SecondComponent
